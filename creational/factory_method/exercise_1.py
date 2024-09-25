82c5b8f376461c94619ab4526e803e552086066d
Крилл Васильев <rubyroid11@yandex.ru>
2024-09-26 00:30:06 +0300

    factory method

--- creational/factory_method/exercise_1.py
+++ creational/factory_method/exercise_1.py
@@ -1,21 +1,43 @@
-class ConcreteProduct1:
+from abc import ABC, abstractmethod
+
+
+class Product(ABC):
+    @abstractmethod
+    def getInfo(self): pass
+
+    @abstractmethod
+    def transform(self): pass
+
+
+class ConcreteProduct1(Product):
     def __init__(self, info):
-        pass
-        # Implement the "constructor"
+        self.info = info.lower()
 
     def getInfo(self):
-        pass
-        # Implement the method
+        return self.info
 
-    def transform(self):
-        pass
-        # Implement the method
+    def transform(self) -> None:
+        t = [c for c in self.info if c != ' ']
+        self.info = ' '.join(t)
 
-# Implement the ConcreteProduct2 class
+
+class ConcreteProduct2(Product):
+    def __init__(self, info):
+        self.info = info.upper()
+
+    def getInfo(self):
+        return self.info
+
+    def transform(self) -> None:
+        t = [c for c in self.info if c != '*' and c != ' ']
+        self.info = '**'.join(t)
 
 
 class Creator:
-    def anOperation(self, info):
+    def factoryMethod(self, info) -> Product:
+        pass
+
+    def anOperation(self, info) -> str:
         p = self.factoryMethod(info)
         p.transform()
         p.transform()
@@ -23,10 +45,10 @@ class Creator:
 
 
 class ConcreteCreator1(Creator):
-    def factoryMethod(self, info):
-        pass
-        # Implement the method
+    def factoryMethod(self, info) -> ConcreteProduct1:
+        return ConcreteProduct1(info)
 
-# Implement the ConcreteCreator2 descendant class;
-#   the anOperation method should not be
-#   overridden in this class
+
+class ConcreteCreator2(Creator):
+    def factoryMethod(self, info) -> ConcreteProduct2:
+        return ConcreteProduct2(info)
