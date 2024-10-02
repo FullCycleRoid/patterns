6c7ccc041ed3aeb74d4cfb4ee0ec5471c6e3429c
Крилл Васильев <rubyroid11@yandex.ru>
2024-10-02 23:23:17 +0300

    factory method 2

--- creational/factory_method/exercise_2.py
+++ creational/factory_method/exercise_2.py
@@ -1,29 +1,40 @@
 class ConcreteProduct1:
     def __init__(self, info):
-        pass
-        # Implement the "constructor"
+        self.info = info.lower()
 
     def getInfo(self):
-        pass
-        # Implement the method
+        return self.info
 
-    def transform(self):
-        pass
-        # Implement the method
+    def transform(self) -> None:
+        t = [c for c in self.info if c != '*' and c != ' ']
+        self.info = '*'.join(t)
 
-# Implement the ConcreteProduct2 descendant class
+
+class ConcreteProduct2(ConcreteProduct1):
+    def __init__(self, info):
+        super().__init__(info)
+        self.info = self.info.capitalize()
+
+    def getInfo(self):
+        return self.info
+
+    def transform(self) -> None:
+        if not self.info.startswith("=="):
+            super().transform()
+            self.info = "==" + self.info + "=="
 
 
 class ConcreteCreator1:
-    def factoryMethod(self, info):
+    def factoryMethod(self, info) -> ConcreteProduct1:
         return ConcreteProduct1(info)
 
-    def anOperation(self, info):
+    def anOperation(self, info) -> str:
         p = self.factoryMethod(info)
         p.transform()
         p.transform()
         return p.getInfo()
 
 
-
-# Implement the ConcreteCreator2 descendant class
+class ConcreteCreator2(ConcreteCreator1):
+    def factoryMethod(self, info) -> ConcreteProduct2:
+        return ConcreteProduct2(info)
