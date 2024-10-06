8ee803c52b92df9f7c27c107cd1678db266aae67
Крилл Васильев <rubyroid11@yandex.ru>
2024-10-07 00:56:42 +0300

    prototype 2 + test

--- creational/prototype/exercise_2.py
+++ creational/prototype/exercise_2.py
@@ -1,28 +1,55 @@
-class Ellip:
-    def __init__(self):
-        pass
-        # Implement the "constructor"
+from abc import ABC
+from copy import deepcopy
+
+
+class AbstractGraphic(ABC):
+    def __init__(self, x1: int = 0, y1: int = 0, x2: int = 0, y2: int = 0):
+        self.x1: int = x1
+        self.x2: int = x2
+        self.y1: int = y1
+        self.y2: int = y2
+
+    def clone(self):
+        return deepcopy(self)
 
     def changeLocation(self, x1, y1, x2, y2):
-        pass
-        # Implement the method
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
 
     def draw(self):
-        pass
-        # Implement the method
+        return f'{self.__class__.__name__}({self.x1},{self.y1},{self.x2},{self.y2})'
 
-# Implement the Line and Rect classes
+
+class Ellipse(AbstractGraphic):
+    pass
+
+
+class Line(AbstractGraphic):
+    pass
+
+
+class Rect(AbstractGraphic):
+    pass
 
 
 class GraphEditor:
-    def __init__(self, p0, p1):
-        pass
-        # Implement the "constructor"
+    def __init__(self, p0: AbstractGraphic, p1: AbstractGraphic):
+        self.p0 = p0
+        self.p1 = p1
+        self.store = []
 
     def addGraphic(self, ind, x1, y1, x2, y2):
-        pass
-        # Implement the method
+        tmp_obj = None
+        if ind == 0:
+            tmp_obj = self.p0.clone()
+        if ind == 1:
+            tmp_obj = self.p1.clone()
+
+        tmp_obj.changeLocation(x1, y1, x2, y2)
+        self.store.append(tmp_obj)
 
     def drawAll(self):
-        pass
-        # Implement the method
+        l = [el.draw() for el in self.store]
+        return ' '.join(l)
--- tests/creational/test_prototype.py
+++ tests/creational/test_prototype.py
@@ -1,6 +1,7 @@
 import pytest
 
 from creational.prototype.exercise_1 import Client, ConcretePrototype1, ConcretePrototype2
+from creational.prototype.exercise_2 import Rect, Line, Ellipse, GraphEditor
 
 
 @pytest.mark.parametrize(
@@ -13,7 +14,7 @@ from creational.prototype.exercise_1 import Client, ConcretePrototype1, Concrete
          ),
     ]
 )
-def test_factory_method_exersice1_1(word, numbers, results):
+def test_prototype_1(word, numbers, results):
     client1 = Client(ConcretePrototype1(data=word, id=numbers[0]))
     client2 = Client(ConcretePrototype2(data=word, id=numbers[0]))
 
@@ -23,3 +24,36 @@ def test_factory_method_exersice1_1(word, numbers, results):
 
     assert client1.getObjects() == results[0]
     assert client2.getObjects() == results[1]
+
+
+@pytest.mark.parametrize(
+    "p, objs, result",
+    [
+        (
+            "RL",
+            [
+                (1, 1, 1, 3, 1),
+                (0, 3, -2, 2, 3),
+                (1, -2, 2, 4, -1),
+                (0, 1, 2, 2, 4),
+                (0, 2, 0, 4, 0)
+            ],
+             "Line(1,1,3,1) Rect(3,-2,2,3) Line(-2,2,4,-1) Rect(1,2,2,4) Rect(2,0,4,0)"
+        )
+    ]
+)
+def test_prototype_2(p, objs, result):
+    types = {
+        "R": Rect,
+        "L": Line,
+        "E": Ellipse
+    }
+    p0 = p[0]
+    p1 = p[1]
+
+    ge = GraphEditor(types[p0](), types[p1]())
+
+    for obj in objs:
+        ge.addGraphic(*obj)
+
+    assert ge.drawAll() == result
