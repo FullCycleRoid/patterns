4417929aebec840f31f9db775e45dee74fad91ab
Крилл Васильев <rubyroid11@yandex.ru>
2024-10-07 22:07:03 +0300

    Singleton

--- creational/singleton/exercise_1.py
+++ creational/singleton/exercise_1.py
@@ -1,3 +1,12 @@
+class METASingleton(type):
+    __uniqueInstance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls != cls.__uniqueInstance:
+            cls.__uniqueInstance = super(METASingleton, cls).__call__(*args, **kwargs)
+        return cls.__uniqueInstance[cls]
+
+
 class BaseClass:
     def __init__(self):
         self.__data = 0
@@ -8,18 +17,26 @@ class BaseClass:
     def getData(self):
         return self.__data
 
-class Singleton(BaseClass):
+
+class Singleton(BaseClass, metaclass=METASingleton):
     __private_key = object()
-    __uniqueInstance = None
 
     def __init__(self, private_key):
         if private_key != self.__private_key:
             raise Exception("Error")
         super().__init__()
 
-    # Complete the implementation of the class
 
-class Doubleton(BaseClass):
+class METADoubleton(type):
+    __instances = [None] * 2
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super(METADoubleton, cls).__call__(*args, **kwargs)
+        return cls.__instances[cls]
+
+
+class Doubleton(BaseClass, metaclass=METADoubleton):
     __private_key = object()
     __instances = [None] * 2
 
@@ -28,9 +45,17 @@ class Doubleton(BaseClass):
             raise Exception("Error")
         super().__init__()
 
-    # Complete the implementation of the class
 
-class Tenton(BaseClass):
+class METATenton(type):
+    __instances = [None] * 10
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super(METATenton, cls).__call__(*args, **kwargs)
+        return cls.__instances[cls]
+
+
+class Tenton(BaseClass, metaclass=METATenton):
     __private_key = object()
     __instances = [None] * 10
 
@@ -38,5 +63,3 @@ class Tenton(BaseClass):
         if private_key != self.__private_key:
             raise Exception("Error")
         super().__init__()
-
-    # Complete the implementation of the class
