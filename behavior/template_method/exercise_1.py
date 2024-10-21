ba72c36955bf3fa6be8c5a7c137d596fd02b4132
Крилл Васильев <rubyroid11@yandex.ru>
2024-10-21 17:23:02 +0300

    Template method

--- behavior/template_method/exercise_1.py
+++ behavior/template_method/exercise_1.py
@@ -1,7 +1,45 @@
-class AbstractClass:
-    pass
-    # Implement methods templateMethod,
-    # basicOperation1, basicOperation2 and hookOperation
+from abc import abstractmethod
 
-# Implement the ConcreteClass1, ConcreteClass2, ConcreteClass3
-#   and ConcreteClass4 descendant classes
+
+class AbstractClass:
+    def BasicOperation1(self):
+        return "Boil water"
+
+    @abstractmethod
+    def PrimitiveOperation(self):
+        return ""
+
+    def BasicOperation2(self):
+        return "=Pour into a cup"
+
+    def _HookOperation(self):
+        return ""
+
+    def templateMethod(self):
+        drink = ''
+        drink += self.BasicOperation1()
+        drink += self.PrimitiveOperation()
+        drink += self.BasicOperation2()
+        drink += self._HookOperation()
+        return drink
+
+
+class ConcreteClass1(AbstractClass):
+
+    def PrimitiveOperation(self):
+        return "=Brew tea"
+
+
+class ConcreteClass2(AbstractClass):
+    def PrimitiveOperation(self):
+        return "=Brew coffee"
+
+
+class ConcreteClass3(ConcreteClass1):
+    def _HookOperation(self):
+        return "=Add sugar and lemon»"
+
+
+class ConcreteClass4(ConcreteClass2):
+    def _HookOperation(self):
+        return "=Add sugar and milk"
