d4ce061fd0246dc041484537b5ded8a666310915
Крилл Васильев <rubyroid11@yandex.ru>
2024-10-21 19:31:21 +0300

    Template method

--- behavior/template_method/exercise_2.py
+++ behavior/template_method/exercise_2.py
@@ -1,7 +1,52 @@
-class AbstractComparable:
-    pass
-    # Implement the indexMax, lastIndexMax, indexMin
-    #   and lastIndexMin static methods
+from abc import abstractmethod
+from typing import Sequence
 
-# Implement the NumberComparable, LengthComparable
-#   and TextComparable descendant classes
+
+class AbstractComparable:
+
+    @abstractmethod
+    def compareTo(self, other):
+        pass
+
+    @staticmethod
+    def indexMax(comp: Sequence['AbstractComparable']) -> int:
+        pass
+
+    @staticmethod
+    def indexMin(comp: Sequence['AbstractComparable']) -> int:
+        pass
+
+    @staticmethod
+    def lastIndexMin(comp: Sequence['AbstractComparable']) -> int:
+        pass
+
+    @staticmethod
+    def lastIndexMax(comp: Sequence['AbstractComparable']) -> int:
+        pass
+
+
+class NumberComparable(AbstractComparable):
+    def __init__(self, data: int | str):
+        if not data.isdigit():
+            data = 0
+
+        self.key: int = int(data)
+
+    def compareTo(self, other):
+        pass
+
+
+class LastNumberComparable(AbstractComparable):
+    def __init__(self, data: int | str):
+        self.key = len(data)
+
+    def compareTo(self, other):
+        pass
+
+
+class TextComparable(AbstractComparable):
+    def __init__(self, data: int | str):
+        self.key = str(data)
+
+    def compareTo(self, other):
+        pass
