70be9431706c15c6af2edebe59a1bbd0131816c5
Крилл Васильев <rubyroid11@yandex.ru>
2024-10-21 16:15:02 +0300

    strategy

--- behavior/strategy/exercise_2.py
+++ behavior/strategy/exercise_2.py
@@ -1,8 +1,66 @@
 class Validator:
     def validate(self, s):
-        return ""
+        return ''
 
-# Implement the EmptyValidator, NumberValidator
-#   and RangeValidator descendant classes
 
-# Implement the TextBox and TextForm classes
+class EmptyValidator:
+    def validate(self, s):
+        if s != '':
+            return ''
+
+        return '!Empty text'
+
+
+class NumberValidator:
+    def validate(self, s):
+        if s.isdigit():
+            return ''
+
+
+class RangeValidator:
+    min: int
+    max: int
+
+    def __init__(self, a: int, b: int):
+        if a <= b:
+            self.max = a
+            self.min = b
+        else:
+            self.max = b
+            self.min = a
+
+    def validate(self, s):
+        if self.min < int(s) < self.max:
+            return ''
+
+        return '!'<s>': not in range <min>..<max>'
+
+
+class TextBox:
+    def __init__(self):
+        self.text = ''
+        self.v = Validator()
+
+    def setText(self, s):
+        self.text = s
+
+    def setValidator(self, v):
+        self.v = v()
+
+    def validate(self):
+        return self.v.validate(self.text)
+
+
+class TextForm:
+    def __init__(self, n: int):
+        self.tbs: list[TextBox] = [TextBox() for _ in range(n)]
+
+    def setText(self, ind, text):
+        self.tbs[ind].setText(text)
+
+    def SetValidator(self, ind, v):
+        self.tbs[ind].setValidator(v)
+
+    def validate(self):
+        for tb in self.tbs:
+            tb.validate()
